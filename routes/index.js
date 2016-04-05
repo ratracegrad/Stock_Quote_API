71c0e2cf88acb31018bfb013d60bae71ad42e4b5
Jennifer Bland <ratracegrad@gmail.com>
2016-04-05 14:35:55 -0400

    Updated styling on the home page to add drop shadow to box. Removed headers from request call and removed items that are no longer needed

--- routes/index.js
+++ routes/index.js
@@ -16,21 +16,14 @@ module.exports = function(app) {
         var options = {
             url: 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json',
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            //headers: {
+            //    'Content-Type': 'application/json'
+            //},
             qs: query
         }
 
         request(options, function(err, request, body) {
-
-            if (request.statusCode === 200 && body.Message) {
-                request.statusCode = 400;
-                return res.status(400).json(new Error(body.Message));
-            } else {
-                res.json(body);
-            }
-
+            res.json(body);
         });
     });
 
