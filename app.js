1d6bea51a3c45b79c55403af6ec4f5cbd875e383
Jennifer Bland <ratracegrad@gmail.com>
2016-04-05 17:35:02 -0400

    Added connection to mongoose database. Added all associated error catching with mongoose too

--- app.js
+++ app.js
@@ -7,31 +7,64 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var routes = require('./routes/index.js');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+
+// ================================================================
+// get environment variables and start server
+// ================================================================
+dotenv.load();
 var port = process.env.PORT || 3000;
+var dbURI = process.env.MONGODB || process.env.MONGOLAB_URI
 
 var app = express();
 
 
 // ================================================================
-// setup our express application
+// Connect to database (only start server if connected)
 // ================================================================
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use(express.static('public'));
-app.set('views', './views');
-app.set('view engine', 'ejs');
+mongoose.connect(dbURI);
+mongoose.connection.on('error', function() {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+
+    // ================================================================
+    // setup our express application
+    // ================================================================
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(cookieParser());
+    app.use(express.static('public'));
+    app.set('views', './views');
+    app.set('view engine', 'ejs');
 
 
-// ================================================================
-// setup routes
-// ================================================================
-routes(app);
+    // ================================================================
+    // setup routes
+    // ================================================================
+    routes(app);
 
 
-// ================================================================
-// start our server
-// ================================================================
-app.listen(port, function(err) {
-    console.log('Server running on port: ' + port);
+    // ================================================================
+    // start our server
+    // ================================================================
+    app.listen(port, function(err) {
+        console.log('Server running on port: ' + port);
+    });
 });
